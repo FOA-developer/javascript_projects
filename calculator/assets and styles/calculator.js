c389a8705ebe45b3aad8c575ebc97a94ed63c6bc
FOA-developer <flourishagbanoma@gmail.com>
2026-02-10 13:13:27 +0100

    optimized the script

--- calculator/assets and styles/calculator.js
+++ calculator/assets and styles/calculator.js
@@ -1,37 +1,31 @@
-let screen = document.querySelector('.input');
+// display input 
+  let screen = document.querySelector('.input');
 
-function displayButton(name){
-  let display = document.querySelector(`.${name}`);
-  countLength();
-  screen.value += display.innerText;
-  return screen.value;
-}
+  let button = document.querySelectorAll('.display');
+    button.forEach((one) => {
+      one.addEventListener('click', () => {
+        countLength();
+        screen.value += one.innerText;
+        return screen.value;
+      })
+    })
 
-function countLength(){
-  let count = screen.value.length;
-  if(count === 7){
-    clearElement();
+// counts length in the screen
+  function countLength(){
+    let count = screen.value.length;
+    if(count === 7){
+      clearElement();
+    }
   }
-}
-document.querySelector('.input').addEventListener('keydown', () => {
- countLength()
-})
 
-function clearElement(){
-  let newValue = screen.value.slice(0, -1);
-  screen.value = newValue;
-}
+// invokes the count length anytime a buton is clicked
+  document.querySelector('.input').addEventListener('keydown', () => {
+  countLength()
+  })
 
-document.querySelector('.clear').addEventListener('click', () => {clearElement();})
-
-/*function calculate(){
-  let exp = Number(screen.value);
-  let result = eval(exp)
-  console.log(result)
-  console.log(typeof(exp))
-  return result;
-}*/
-
-function covertToDecimal(){
-
-}
+// clears a value from the screen 
+  function clearElement(){
+    let newValue = screen.value.slice(0, -1);
+    screen.value = newValue;
+  }
+  document.querySelector('.clear').addEventListener('click', () => clearElement())
