e45d8a59dc951e20c4c65541d5e0c6910b1fda7e
FOA-developer <flourishagbanoma@gmail.com>
2026-04-19 23:21:41 +0100

    making it able to solve complex equations

--- calculator/assets and styles/calculator.js
+++ calculator/assets and styles/calculator.js
@@ -30,6 +30,8 @@
   }
   document.querySelector('.clear').addEventListener('click', () => clearElement())
 
+
+/*
 //declaring operators
 const operators = ['+', '-', 'x', '/'];
 
@@ -74,4 +76,75 @@ let secondValue = '';
 
   }
 // invoking the calculate function when the equal button is clicked
-  document.querySelector('.equal').addEventListener('click', () => calculate())
+*/
+
+const operators = ['+', '-', 'x', '/'];
+const calculate = () => {
+  let tokens = [];
+  let num = '';
+  for(let i = 0; i < screen.value.length; i++){
+    let isOperator = operators.includes(screen.value[i]);
+    if(isOperator == false){
+      num += screen.value[i];
+      console.log(num)
+    }else{
+      tokens.push(Number(num));
+      tokens.push(screen.value[i]);
+      num = '';
+    };
+  }
+  if (tokens[tokens.length -1]){
+    if(num !== ''){
+      tokens.push(Number(num));
+    }
+   }
+  
+  console.log(tokens);
+
+  const cal = () => {
+      let bodmas = tokens.findIndex((op) => op === "/")
+      if(bodmas !== -1){
+        valueCal(bodmas, tokens);
+        tokens[bodmas - 2] = values[0] / values[1];
+        console.log(bodmas)
+        console.log(values)
+        console.log(tokens[bodmas-2])
+      }else{
+        let odmas = tokens.findIndex((op) => op === "x");
+        if(odmas !== -1){
+          valueCal(odmas, tokens);
+          tokens[odmas - 2] = values[0] * values[1];
+        }else{
+            let bodmas = tokens.findIndex((op) => op === "+");
+            if(bodmas !== -1){
+            valueCal(bodmas, tokens);
+            tokens[bodmas - 2] = values[0] + values[1];
+            }else{
+              let bodmas = tokens.findIndex((op) => op === "-")
+              if(bodmas !== -1){
+                valueCal(bodmas, tokens);
+                tokens[bodmas - 2] = values[0] - values[1];
+              }
+            }
+        }
+
+    }
+    console.log(tokens);
+  }
+  
+  cal();
+
+}
+
+const valueCal = (index, arr) => {
+  values = arr.slice(index - 1, index + 2);
+
+  return values;
+}
+
+document.querySelector('.equal').addEventListener('click', () => calculate());
+
+const toDecimal = () => {
+  screen.value = parseFloat(screen.value);
+}
+document.querySelector('.dec').addEventListener('click', () => toDecimal())
