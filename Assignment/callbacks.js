7875d44a3bea37c8f366042d01360afcc3444f7f
FOA-developer <flourishagbanoma@gmail.com>
2026-04-21 00:37:37 +0100

    did a thing on promise in js

--- Assignment/callbacks.js
+++ Assignment/callbacks.js
@@ -4,35 +4,56 @@ const correctUsername = 'flourish';
 const correctPassword = '12345';
 
 
-const login = (username, password, callback) =>{
-  let verdict = "";
+const login = (username, password) =>{
+  return new Promise((resolve, reject) => {
+    let verdict = "";
     setTimeout(() => {
     if(username === correctUsername){
       if(password === correctPassword){
-         verdict = "successful"
+         resolve("Login successful")
       }else{
-        verdict = "wrong password"
+        reject("wrong password")
       }
     }else{
-      verdict = "invalid username"
+      reject("invalid username...Try Again")
     }
-    callback(verdict,username);
   }, 2000)
+  })
 }
 
-
-const displayStatus = (verdict,user) => {
-  if(verdict === "successful"){
-    console.log(`Login Succseeful...Welcome Back ${user}`);
-  }else if(verdict === "wrong password"){
-    console.log(`Wrong Password...Try Again`)
-  }else if(verdict === "invalid username"){
-    console.log("Invalid Username...Try Again")
-  }
-}
 
 btn.addEventListener('click', () => {
   const username = document.querySelector('.user').value
   const password = document.querySelector('.special').value
-  login(username, password, displayStatus)
+  login(username, password)
+   .then(message => {console.log(`${message}...Welcome Back ${user}`)})
+   .catch(message => console.log(message))
 })
+
+const displayStatus = (verdict,user) => {
+  if(verdict === "successful"){
+    console.log();
+  }else if(verdict === "wrong password"){
+    console.log(`Wrong Password...Try Again`)
+  }else if(verdict === "invalid username"){
+    console.log("Invalid Username")
+  }
+}
+
+
+
+
+const testPromise = new Promise((resolve,reject) => {
+  const result = 5 + 5;
+  if(result === 10){
+    resolve("Fulfilled")
+  }else{
+    reject("Rejected")
+  }
+});
+
+testPromise.then(message => {
+  console.log(message)
+}).catch(message => {console.log(message)})
+
+
